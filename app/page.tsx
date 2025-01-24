5c9bf0d0bf5a6c8fcc1008f3fae7c97c6fe7854b
Lucas Ramos <lucas.ramos@datacom.com.br>
2025-01-23 23:19:14 -0300

    Add eslint and prettier rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,53 @@
 'use client'
 
-import Image from 'next/image';
-import { Button } from '@/components';
-import Countdown from 'react-countdown';
-import { useRef } from 'react';
+import Image from 'next/image'
+import { Button } from '@/components'
+import Countdown from 'react-countdown'
+import { useRef } from 'react'
 
 // Random component
-const Completionist = () => <span>You are good to go!</span>;
+const Completionist = () => <span>You are good to go!</span>
 
 // Renderer callback with condition
 const renderer = ({ minutes, seconds, completed }) => {
   if (completed) {
     // Render a complete state
-    return <Completionist />;
+    return <Completionist />
   } else {
     // Render a countdown with format 00:00
-    const formatNumber = (num) => num.toString().padStart(2, '0');
+    const formatNumber = (num) => num.toString().padStart(2, '0')
     return (
       <span>
         {formatNumber(minutes)}:{formatNumber(seconds)}
       </span>
-    );
+    )
   }
-};
+}
 
 export default function Home() {
-  const countdownRef = useRef(null);
+  const countdownRef = useRef(null)
 
   const handleStartClick = () => {
     if (countdownRef.current) {
-      countdownRef.current.start();
+      countdownRef.current.start()
     }
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-red-500 w-4/5 h-96 rounded-lg shadow-lg grid grid-cols-3 grid-rows-3 gap-4">
-        <h1 className="text-white text-2xl mb-4 col-start-2 row-start-1 text-center">Focus Cycle</h1>
-        <div className="col-start-3 row-start-1 justify-self-end mt-3 mr-3">
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="grid h-96 w-4/5 grid-cols-3 grid-rows-3 gap-4 rounded-lg bg-red-500 shadow-lg">
+        <h1 className="col-start-2 row-start-1 mb-4 text-center text-2xl text-white">
+          Focus Cycle
+        </h1>
+        <div className="col-start-3 row-start-1 mr-3 mt-3 justify-self-end">
           <Button
-            svg={<Image alt="test" src="/adjustments.svg" width={24} height={24} />}
+            svg={
+              <Image alt="test" src="/adjustments.svg" width={24} height={24} />
+            }
             onClick={() => alert('adjustments menu')}
           />
-        </ div>
-        <div className="col-start-2 row-start-2 justify-self-center self-center text-6xl">
+        </div>
+        <div className="col-start-2 row-start-2 self-center justify-self-center text-6xl">
           <Countdown
             ref={countdownRef}
             date={Date.now() + 1500000}
@@ -51,20 +55,27 @@ export default function Home() {
             autoStart={false}
           />
         </div>
-        <div className="col-start-3 row-start-2 justify-self-end self-center mr-3">
+        <div className="col-start-3 row-start-2 mr-3 self-center justify-self-end">
           <Button
-            svg={<Image alt="arrow-rigth" src="/chevron-right.svg" width={24} height={24} />}
+            svg={
+              <Image
+                alt="arrow-rigth"
+                src="/chevron-right.svg"
+                width={24}
+                height={24}
+              />
+            }
             onClick={() => alert('next cycle')}
           />
         </div>
-        <div className="col-start-2 row-start-3 justify-self-center self-end mb-3">
+        <div className="col-start-2 row-start-3 mb-3 self-end justify-self-center">
           <Button
             label="Start"
-            style='bg-gray-800 h-10 px-5 hover:bg-gray-900'
+            style="bg-gray-800 h-10 px-5 hover:bg-gray-900"
             onClick={handleStartClick}
           />
         </div>
       </div>
     </div>
-  );
+  )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from 'react'
 
 interface ButtonProps {
-    label?: string;
-    svg?: React.ReactNode;
-    style?: string;
-    onClick?: () => void;
+  label?: string
+  svg?: React.ReactNode
+  style?: string
+  onClick?: () => void
 }
 
 const Button: React.FC<ButtonProps> = ({ label, svg, style, onClick }) => {
-    return (
-        <button onClick={onClick} className={`button rounded-md ${style}`} type='button'>
-            {svg ? svg : label}
-        </button>
-    );
-};
+  return (
+    <button
+      onClick={onClick}
+      className={`button rounded-md ${style}`}
+      type="button"
+    >
+      {svg || label}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
