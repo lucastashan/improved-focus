efc8f98f9f017fc5ec025fabbf620264d10f3f4f
Lucas Ramos <lucas.ramos@datacom.com.br>
2025-02-09 22:38:27 -0300

    add distraction list skeleton

--- components/Container.tsx
+++ components/Container.tsx
@@ -10,11 +10,21 @@ export default function Container({
   hasCols = true,
 }: ContainerProps) {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen flex-col justify-center">
       <div
-        className={`grid h-96 w-4/5 ${hasCols ? 'grid-cols-3' : 'grid-cols-1'} grid-rows-3 gap-4 rounded-lg bg-red-500 shadow-lg`}
+        className={`mx-auto grid h-96 w-4/5 ${hasCols ? 'grid-cols-3' : 'grid-cols-1'} grid-rows-3 gap-4 rounded-lg bg-red-500 shadow-lg`}
       >
         {children}
+      </div>
+      <div className="mt-4 flex flex-col">
+        <h1 className="mx-[5%] mb-2 whitespace-nowrap border-b">
+          Distraction history:
+        </h1>
+        <ul className="ml-[8%] space-y-1">
+          <li>Distraction 1 (Times: 3)</li>
+          <li>Distraction 2</li>
+          <li>Distraction 3</li>
+        </ul>
       </div>
     </div>
   )
